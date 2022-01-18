269a54779fd634542816f251368e1633437daa3d
Shahzaib-ur-Rehman <shaahzaibrehman@gmail.com>
2022-01-18 19:01:31 +0500

    slider added

---
+++ components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+
+const Banner = () => {
+  return (
+    <AwesomeSlider>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </AwesomeSlider>
+  );
+};
+
+export default Banner;
